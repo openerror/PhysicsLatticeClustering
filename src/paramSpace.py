080eb644f2071e50bcb5674d7225ef25b3220674
Everest Law <everestl@usc.edu>
2019-01-20 00:43:17 -0800

    Removed extraneous paramSpace.py

--- src/paramSpace.py
+++
@@ -1,4 +0,0 @@
-thresholds = (0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1)
-simParams_dict = {"capacity": 100, "sizeY": 150, "sizeX": 150}
-directory_dict = {"root": "/home/everestlaw/Desktop/latticeClusterCount/occupancyTracker",
-                  "data": "data", "output": "output"}
